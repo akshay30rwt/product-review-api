403580ed952600c430c7ec43dd4e6190c7091395
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-02 23:42:46 +0530

    fix: fix constant name in login

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,7 +54,7 @@ const login = async (req, res) => {
         }
 
         const token = jwt.sign(
-            { userId: existingUser._id},
+            { userId: user._id},
             process.env.JWT_SECRET,
             { expiresIn: '1d'}
         );
