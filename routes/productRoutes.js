2d30fcdc7480adcba8056d9b8c9a494f1d0a5241
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-02 23:29:34 +0530

    feat: implement productRoutes

---
+++ routes/productRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { addProduct, getAllProducts, getProductById, updateProduct, deleteProduct, addProductReview, getProductReview } = require('../controllers/productController');
+const protect = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.post('/',protect, addProduct);
+router.get('/', getAllProducts);
+router.get('/:id', getProductById);
+router.put('/:id',protect, updateProduct);
+router.delete('/:id',protect, deleteProduct);
+router.post('/:id/reviews', addProductReview);
+router.get('/:id/reviews', getProductReview);
+
+module.exports = router;
